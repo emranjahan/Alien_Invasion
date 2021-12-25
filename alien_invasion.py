573ad9dd806328435f12e108b1ab2496bfc955ec
Md. Emran Jahan <emranjahan7@gmail.com>
2021-12-25 16:27:54 +0600

    Make Setting in this step

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,16 @@ import sys
 
 import pygame 
 
+from settings import Settings
+
 def run_game():
-    # Initialize game and create a screen object.
+    # Initialize pygame, settings and screen object.
     pygame.init()
-    screen = pygame.display.set_mode((800, 400))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_height, ai_settings.screen_width))
     pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
+
 
     # Start the main loop for the game.
     while True:
@@ -15,6 +20,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        
+        # Redraw the scree during each pass through the loop.
+        screen.fill(bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings():
+    """ A class to store all settings for alien invasion."""
+
+    def __init__(self):
+
+        """ Initialize the game's setting."""
+        # Display setting.
+
+        self.screen_width = 400
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
