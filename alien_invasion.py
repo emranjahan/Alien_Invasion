fdae01f0f346de01c4faa04127efeee9aa8196bc
Md. Emran Jahan <emranjahan7@gmail.com>
2021-12-25 16:54:57 +0600

    image loaded

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,7 +10,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_height, ai_settings.screen_width))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230, 230, 230)
+ 
 
 
     # Start the main loop for the game.
@@ -22,7 +22,7 @@ def run_game():
                 sys.exit()
         
         # Redraw the scree during each pass through the loop.
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
